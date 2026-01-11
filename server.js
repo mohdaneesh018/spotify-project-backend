198e9c19e75157220f8e2e126c14096bc381e3ad
mohdaneesh018 <mdaneesh537@gmail.com>
2026-01-12 00:09:19 +0530

    URL Updates

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set("trust proxy", 1);
 
 app.use(cors({
     // origin: "http://localhost:5173",
-    origin: ["awdiz-project.vercel.app", "http://localhost:5173"],
+    origin: ["https://awdiz-project.vercel.app", "http://localhost:5173"],
     credentials: true,
 }));
 app.use(cookieParser());
